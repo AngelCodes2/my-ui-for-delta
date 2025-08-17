e458819ddf9500ea9f5e41843a58dbb4aad07149
AngelCodes2 <164281726+AngelCodes2@users.noreply.github.com>
2025-08-17 10:50:39 -0500

    Create dexStuff.lua

---
+++ dexStuff.lua
@@ -0,0 +1,10 @@
+local StarterGui = game:GetService("StarterGui")
+local DexGui = Instance.new("ScreenGui")
+DexGui.Parent = StarterGui
+DexGui.Name = "DexGui"
+
+local DexFrame = Instance.new("Frame")
+DexFrame.Parent = DexGui
+DexFrame.Name = "DexFrame"
+ 
+DexFrame.Size = UDim2.new(1,0,0.1,0) --work in progress stuff
